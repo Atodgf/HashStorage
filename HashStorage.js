9d42ea7f0625a960b11d4bc52922a8ec288e6372
fgdota2 <68871109+fgdota2@users.noreply.github.com>
2020-12-03 10:43:35 +0300

    123

--- HashStorage.js
+++ HashStorage.js
@@ -1,40 +1,105 @@
 class HashStorage {
     constructor() {
-      this.store = {}; // хранилище для значений
+      this.store_ = {}; 
     }
   
     add(key, value) {
-      this.store[key] = value; // сохраняем значение в store
+      this.store_[key] = value; 
     }
   
     getValue(key) {
-      return this.store[key]; // тут все тоже просто и нативно
+      if(!(key in this.store_)) {
+        return ('Такого напитка нет в списке!')
+      }
+
+      return this.store_[key]; 
     }
   
     deleteValue(key) {
-      if(!(key in this.store)) {
+      if(!(key in this.store_)) {
         return false
       }
   
-      return delete this.store[key];
+      return delete this.store_[key];
     }
   
     getKeys() {
-      return Object.keys(this.store);
+      return Object.keys(this.store_);
     }
   }
   
   const coctailsStorage = new HashStorage();
+
   
-  // Дальше буду описываться кнопки
+  coctailsStorage.add('vodka', {name: 'vodka', isAlcohol: true}); 
+  coctailsStorage.add('tea', {name: 'tea',isAlcohol: false}); 
+  
+  // Кнопка 'Добавить'
   const addButton = document.getElementById('add-coctail');
   addButton.onclick = function () {
-    const coctail = {};
-    // получаем данные от пользовател
+   
+   
     const name = window.prompt('Введите название коктеля');
     const isAlcohol = window.prompt('Напиток алкогольный?');
     
-    // Добавляем значение в наше хранилище
+     
     coctailsStorage.add(name, {name, isAlcohol});
     console.log('Coctail was added', coctailsStorage.getValue(name));
+ }
+
+
+
+// Кнопка 'Рецепт'
+const checkButton = document.getElementById('get-coctail');
+checkButton.onclick = function () {
+  
+  const name = window.prompt('Введите название коктеля');
+  
+  const display = document.querySelector('h1')
+
+  display.textContent = coctailsStorage.getValue(name)
+
+
+  
 }
+
+// Кнопка 'Удалить'
+const delButton = document.getElementById('delete-coctail');
+  delButton.onclick = function () {
+     
+    const name = window.prompt('Введите название коктеля');
+
+    coctailsStorage.deleteValue(name);
+    
+ }
+
+// Кнопка 'Список Рецептов'
+const recButton = document.getElementById('all-coctails');
+  recButton.onclick = function () {
+
+    const display = document.querySelector('textarea')
+
+    display.textContent = coctailsStorage.getKeys();
+    
+  }
+
+  // // Кнопка 'Рецепт'
+  // const checkButton = document.getElementById('get-coctail');
+  //   checkButton.onclick = function () {
+  //     let checkName = window.prompt('Введите название коктеля');
+  //     for (let key in coctailsStorage.store_) {
+  //       if (checkName == key) {
+  //         alert(Object.values(coctailsStorage.store_))
+  //         break
+  //       }  else {
+  //         alert('Данного напитка нету в списке!')
+  //         break
+  //       }
+  //     }
+  //   }
+      
+    
+ 
+
+      
+  console.log(coctailsStorage)
